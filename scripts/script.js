112179dfce3ec0ed83513a89155f1b56acfb24fa
Giusepe V.R <giusepe.vr@gmail.com>
2025-01-19 22:12:19 -0600

    feat/ clone cards

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,35 @@ const profileAboutInput = document.querySelector(".popup__input_about");
 const storedProfileName = document.querySelector(".profile__name");
 const storedProfileAbout = document.querySelector(".profile__description");
 const formElement = document.querySelector(".popup__form");
+const cardsSection = document.querySelector(".photo-grid");
+const cardTemplate = document.querySelector("#card-template").content;
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
 
 function togglePopup() {
   popUpElement.classList.toggle("popup_opened");
@@ -15,8 +44,8 @@ function togglePopup() {
 }
 
 function renderInputTexts() {
-  let profileName = storedProfileName.textContent;
-  let profileAbout = storedProfileAbout.textContent;
+  const profileName = storedProfileName.textContent;
+  const profileAbout = storedProfileAbout.textContent;
   profileNameInput.value = profileName;
   profileAboutInput.value = profileAbout;
 }
@@ -28,6 +57,20 @@ function handleProfileFormSubmit(evt) {
   togglePopup();
 }
 
+function renderInitialCards(array) {
+  array.forEach((element) => {
+    createCard(element.name, element.link);
+  });
+}
+
+function createCard(nameValue, imageValue) {
+  const newCard = cardTemplate.cloneNode(true);
+  newCard.querySelector(".photo-grid__card-title").textContent = nameValue;
+  newCard.querySelector(".photo-grid__card-image").src = imageValue;
+  cardsSection.append(newCard);
+}
+
 editProfileButton.addEventListener("click", togglePopup);
 closePopupButton.addEventListener("click", togglePopup);
 formElement.addEventListener("submit", handleProfileFormSubmit);
+renderInitialCards(initialCards);
