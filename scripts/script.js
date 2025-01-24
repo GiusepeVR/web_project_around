d9f82f4dd980096b35dc56deccc977e175111dcc
Giusepe V.R <giusepe.vr@gmail.com>
2025-01-23 19:01:33 -0600

    feat/image overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,29 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
-const popUpElement = document.querySelector(".popup");
-const closePopupButton = document.querySelector(".popup__close-button");
+const addPlaceButton = document.querySelector(".profile__add-button");
+const userPopUpElement = document.querySelector("#user-popup");
+const placePopUpElement = document.querySelector("#place-popup");
+const closeUserPopupButton = document.querySelector(".popup__close-button");
+const closePlacePopupButton = document.querySelector("#popup__close-button");
 
 const profileNameInput = document.querySelector(".popup__input_name");
 const profileAboutInput = document.querySelector(".popup__input_about");
+
+const placeNameInput = document.querySelector(".popup__input_place");
+const placeImageLinkInput = document.querySelector(".popup__input_link");
 
 const storedProfileName = document.querySelector(".profile__name");
 const storedProfileAbout = document.querySelector(".profile__description");
 const formElement = document.querySelector(".popup__form");
 const cardsSection = document.querySelector(".photo-grid");
+const mainSection = document.querySelector(".content");
+
 const cardTemplate = document.querySelector("#card-template").content;
+const popupTemplate = document.querySelector("#popup-template").content;
+const popupPrimaryInput = document.querySelector(".popup__input_primary");
+const popupSecondaryInput = document.querySelector(".popup__input_secondary");
+const closeGenericPopupButton = popupTemplate.querySelector(
+  "#popup__generic-close-button"
+);
 
 const initialCards = [
   {
@@ -38,9 +52,32 @@ const initialCards = [
   },
 ];
 
-function togglePopup() {
-  popUpElement.classList.toggle("popup_opened");
+function toggleUserPopup() {
+  userPopUpElement.classList.toggle("popup_opened");
   renderInputTexts();
+}
+
+function togglePlacePopup() {
+  placePopUpElement.classList.toggle("popup_opened");
+}
+
+function toggleGenericPopup(
+  primaryPlaceHolder = "hola",
+  secondaryPlaceHolder = "hola"
+) {
+  const newPopup = popupTemplate.cloneNode(true);
+  const sectionPopup = popupTemplate.querySelector(".popup");
+  const closePopup = popupTemplate.querySelector(
+    "#popup__generic-close-button"
+  );
+
+  closePopup.addEventListener("click", toggleUserPopup);
+  sectionPopup.classList.toggle("popup_opened");
+  newPopup.querySelector(".popup__input_primary").placeholder =
+    primaryPlaceHolder;
+  newPopup.querySelector(".popup__input_secondary").placeholder =
+    secondaryPlaceHolder;
+  mainSection.prepend(newPopup);
 }
 
 function renderInputTexts() {
@@ -70,7 +107,11 @@ function createCard(nameValue, imageValue) {
   cardsSection.append(newCard);
 }
 
-editProfileButton.addEventListener("click", togglePopup);
-closePopupButton.addEventListener("click", togglePopup);
+editProfileButton.addEventListener("click", toggleUserPopup);
+addPlaceButton.addEventListener("click", togglePlacePopup);
+//addPlaceButton.addEventListener("click", toggleGenericPopup);
+closeUserPopupButton.addEventListener("click", toggleUserPopup);
+closeGenericPopupButton.addEventListener("click", toggleGenericPopup);
+closePlacePopupButton.addEventListener("click", togglePlacePopup);
 formElement.addEventListener("submit", handleProfileFormSubmit);
 renderInitialCards(initialCards);
